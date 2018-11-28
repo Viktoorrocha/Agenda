4a0e51210c077305d416a02ca38438e2614e2ad1
Viktor Rocha <viktoorrocha@gmail.com>
2018-11-28 19:25:26 -0400

    utilizando menus

--- app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
+++ app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
@@ -3,6 +3,8 @@ package viktorrocha.br.com.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,5 +25,13 @@ public class FormularioActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
