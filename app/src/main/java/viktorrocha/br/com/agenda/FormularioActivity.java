20bc99f36a9414f469978f3ebca2b60bfbfb9499
Viktor Rocha <viktoorrocha@gmail.com>
2018-11-28 21:45:15 -0400

    Adicionando FormularioHelper

--- app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
+++ app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
@@ -5,26 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import viktorrocha.br.com.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botalSalvar = findViewById(R.id.formulario_salvar);
-        botalSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Aluno Salvo", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
-
+         helper = new FormularioHelper(this);
     }
 
     @Override
@@ -33,5 +31,21 @@ public class FormularioActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_formulario, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+
+                Aluno aluno = helper.pegaAluno();
+
+                Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " Salvo!", Toast.LENGTH_SHORT).show();
+
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
