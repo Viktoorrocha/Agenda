37cf5dfb6abb3ce8f0351034e864301482280823
Viktor Rocha <viktoorrocha@gmail.com>
2018-12-10 17:54:30 -0400

    Permissoes

--- app/src/main/java/viktorrocha/br/com/agenda/ListaAlunosActivity.java
+++ app/src/main/java/viktorrocha/br/com/agenda/ListaAlunosActivity.java
@@ -1,8 +1,12 @@
 package viktorrocha.br.com.agenda;
 
+import android.Manifest.permission;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Browser;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -16,6 +20,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.jar.Manifest;
 
 import viktorrocha.br.com.agenda.dao.AlunoDAO;
 import viktorrocha.br.com.agenda.modelo.Aluno;
@@ -76,6 +81,28 @@ public class ListaAlunosActivity extends AppCompatActivity {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
       final   Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, android.Manifest.permission.CALL_PHONE)
+                       != PackageManager.PERMISSION_GRANTED ) {
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{permission.CALL_PHONE},123);
+                } else {
+
+                    Intent intentLigar =    new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+
+                }
+
+                return false;
+            }
+        });
+
+
         MenuItem itemSMS = menu.add("Enviar SMS");
        Intent intentSMS =  new Intent(Intent.ACTION_VIEW);
       intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
@@ -115,6 +142,5 @@ public class ListaAlunosActivity extends AppCompatActivity {
        });
 
     }
-
 }
 
