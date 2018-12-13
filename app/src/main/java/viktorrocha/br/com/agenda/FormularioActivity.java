e1cc1459d09e7a347ee3acdaf33b8ada71d6142b
Viktor Rocha <viktoorrocha@gmail.com>
2018-12-13 16:35:59 -0400

    Salvando o arquivo de imagem

--- app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
+++ app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
@@ -1,6 +1,9 @@
 package viktorrocha.br.com.agenda;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -20,7 +24,9 @@ import viktorrocha.br.com.agenda.modelo.Aluno;
 
 public class FormularioActivity extends AppCompatActivity {
 
+    public static final int CODIGO_CAMERA = 567;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +49,25 @@ public class FormularioActivity extends AppCompatActivity {
            @Override
            public void onClick(View v) {
            Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-           String caminhoFoto = getExternalFilesDir(null)+ "/" + System.currentTimeMillis() + ".jpg";
+           caminhoFoto = getExternalFilesDir(null)+ "/" + System.currentTimeMillis() + ".jpg";
            File arquivoFoto = new File(caminhoFoto);
            intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
-           startActivity(intentCamera);
+           startActivityForResult(intentCamera,CODIGO_CAMERA);
            }
        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == CODIGO_CAMERA) {
+                helper.carregaImagem(caminhoFoto);
+            }
+        }
+
 
     }
 
@@ -66,16 +85,12 @@ public class FormularioActivity extends AppCompatActivity {
             case R.id.menu_formulario_ok:
                 Aluno aluno = helper.pegaAluno();
                 AlunoDAO dao = new AlunoDAO(this);
-
-
                 if (aluno.getId() != null){
                     dao.altera(aluno);
                 } else {
                     dao.insere(aluno);
                 }
-
                 dao.close();
-
                 finish();
                 break;
         }
--- app/src/main/java/viktorrocha/br/com/agenda/FormularioHelper.java
+++ app/src/main/java/viktorrocha/br/com/agenda/FormularioHelper.java
@@ -1,7 +1,10 @@
 package viktorrocha.br.com.agenda;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Rating;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import viktorrocha.br.com.agenda.modelo.Aluno;
@@ -13,6 +16,7 @@ public class FormularioHelper {
     private final EditText campoTelefone;
     private final EditText campoSite;
     private final RatingBar campoNota;
+    private  final ImageView campoFoto;
 
     private Aluno aluno;
 
@@ -22,6 +26,7 @@ public class FormularioHelper {
         campoTelefone = activity.findViewById(R.id.formulario_telefone);
         campoSite = activity.findViewById(R.id.formulario_site);
         campoNota = activity.findViewById(R.id.formulario_nota);
+        campoFoto = activity.findViewById(R.id.formulario_foto);
         aluno = new Aluno();
     }
 
@@ -31,6 +36,7 @@ public class FormularioHelper {
         aluno.setTelefone(campoTelefone.getText().toString());
         aluno.setSite(campoSite.getText().toString());
         aluno.setNota(Double.valueOf(campoNota.getProgress()));
+        aluno.setCaminhoFoto((String) campoFoto.getTag());
         return  aluno;
     }
 
@@ -40,6 +46,17 @@ public class FormularioHelper {
         campoTelefone.setText(aluno.getTelefone());
         campoSite.setText(aluno.getSite());
         campoNota.setProgress(aluno.getNota().intValue());
+        carregaImagem(aluno.getCaminhoFoto());
         this.aluno = aluno;
+    }
+
+    public void carregaImagem(String caminhoFoto) {
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            campoFoto.setImageBitmap(bitmap);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+            campoFoto.setTag(caminhoFoto);
+        }
     }
 }
