ef24d3e64f85e479cbf452daeaf735b9228d7891
Viktor Rocha <viktoorrocha@gmail.com>
2018-12-12 21:57:47 -0400

    Botao tirar foto funcionando

--- app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
+++ app/src/main/java/viktorrocha/br/com/agenda/FormularioActivity.java
@@ -1,6 +1,8 @@
 package viktorrocha.br.com.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
 
 import viktorrocha.br.com.agenda.dao.AlunoDAO;
 import viktorrocha.br.com.agenda.modelo.Aluno;
@@ -32,6 +36,19 @@ public class FormularioActivity extends AppCompatActivity {
            helper.preencheFormulario(aluno);
        }
 
+
+       Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+
+       botaoFoto.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+           Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+           String caminhoFoto = getExternalFilesDir(null)+ "/" + System.currentTimeMillis() + ".jpg";
+           File arquivoFoto = new File(caminhoFoto);
+           intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+           startActivity(intentCamera);
+           }
+       });
 
     }
 
